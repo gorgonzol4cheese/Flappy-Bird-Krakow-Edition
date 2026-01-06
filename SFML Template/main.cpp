9e086bfb4e9e4fe05f01d9d3ba0c44421e067460
Madzia <magdalena.trepka@vp.pl>
2026-01-06 17:54:13 +0100

    Update main.cpp

--- SFML Template/main.cpp
+++ SFML Template/main.cpp
@@ -1,24 +1,7 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
-
-#define SCREEN_HEIGHT 1024
-#define SCREEN_WIDTH 1024
+using namespace std;
 
 int main() {
-
-	sf::RenderWindow window(sf::VideoMode({ SCREEN_WIDTH,SCREEN_HEIGHT }), "Flappy Bird: Krakow Edition");
-
-	while (window.isOpen()) {
-		//eventy
-
-		//logika gry
-
-		//powstawanie obiektow
-
-		window.clear();
-
-		//dodawanie obiektow SFML
-
-		window.display();
-	}
 	return EXIT_SUCCESS;
 }
